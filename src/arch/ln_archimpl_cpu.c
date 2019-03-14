4dac59500b8e735145b67cb4620407d105145829
Zhao Zhixu <zhixu001@gmail.com>
2019-03-14 11:09:54 +0800

    tested all ep_*.json

--- src/arch/ln_archimpl_cpu.c
+++ src/arch/ln_archimpl_cpu.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Zhao Zhixu
+ * Copyright (c) 2019 Zhao Zhixu
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,87 +22,106 @@
 
 #include "ln_arch.h"
 
-extern ln_op ln_opimpl_reshape_cpu;
-extern ln_op ln_opimpl_create_cpu;
-extern ln_op ln_opimpl_bn2scale_wts_cpu;
-extern ln_op ln_opimpl_upsample_cpu;
+extern ln_op ln_opimpl_arange_cpu;
+extern ln_op ln_opimpl_avgpool2d_cpu;
 extern ln_op ln_opimpl_batchnorm_cpu;
+extern ln_op ln_opimpl_bn2scale_wts_cpu;
 extern ln_op ln_opimpl_concat_cpu;
 extern ln_op ln_opimpl_conv2d_cpu;
+extern ln_op ln_opimpl_create_cpu;
+extern ln_op ln_opimpl_detect_yolov3_cpu;
 extern ln_op ln_opimpl_elew_cpu;
+extern ln_op ln_opimpl_fprint_cpu;
+extern ln_op ln_opimpl_lrelu_cpu;
 extern ln_op ln_opimpl_maxpool2d_cpu;
 extern ln_op ln_opimpl_maxreduce_arg_cpu;
 extern ln_op ln_opimpl_maxreduce_cpu;
+extern ln_op ln_opimpl_pick1d_cpu;
+extern ln_op ln_opimpl_print_cpu;
+extern ln_op ln_opimpl_rearange_cpu;
 extern ln_op ln_opimpl_relu_cpu;
+extern ln_op ln_opimpl_reshape_cpu;
+extern ln_op ln_opimpl_sigmoid_cpu;
 extern ln_op ln_opimpl_slice_cpu;
 extern ln_op ln_opimpl_softmax_cpu;
-extern ln_op ln_opimpl_transpose_cpu;
-extern ln_op ln_opimpl_zeros_cpu;
-extern ln_op ln_opimpl_print_cpu;
-extern ln_op ln_opimpl_sigmoid_cpu;
-extern ln_op ln_opimpl_sort1d_cpu;
-extern ln_op ln_opimpl_arange_cpu;
 extern ln_op ln_opimpl_sort1d_by_key_cpu;
+extern ln_op ln_opimpl_sort1d_cpu;
 extern ln_op ln_opimpl_transform_bboxSQD_cpu;
-extern ln_op ln_opimpl_rearange_cpu;
-extern ln_op ln_opimpl_pick1d_cpu;
-extern ln_op ln_opimpl_fprint_cpu;
-extern ln_op ln_opimpl_lrelu_cpu;
-extern ln_op ln_opimpl_detect_yolov3_cpu;
-extern ln_op ln_opimpl_avgpool2d_cpu;
+extern ln_op ln_opimpl_transpose_cpu;
+extern ln_op ln_opimpl_upsample_cpu;
+extern ln_op ln_opimpl_zeros_cpu;
 /* end of declare cpu ops */
 
-/* TODO: use a hash */
 static ln_op *ops_cpu[] = {
-    &ln_opimpl_reshape_cpu,
-    &ln_opimpl_create_cpu,
-    &ln_opimpl_bn2scale_wts_cpu,
-    &ln_opimpl_upsample_cpu,
+    &ln_opimpl_arange_cpu,
+    &ln_opimpl_avgpool2d_cpu,
     &ln_opimpl_batchnorm_cpu,
+    &ln_opimpl_bn2scale_wts_cpu,
     &ln_opimpl_concat_cpu,
     &ln_opimpl_conv2d_cpu,
+    &ln_opimpl_create_cpu,
+    &ln_opimpl_detect_yolov3_cpu,
     &ln_opimpl_elew_cpu,
+    &ln_opimpl_fprint_cpu,
+    &ln_opimpl_lrelu_cpu,
     &ln_opimpl_maxpool2d_cpu,
     &ln_opimpl_maxreduce_arg_cpu,
     &ln_opimpl_maxreduce_cpu,
+    &ln_opimpl_pick1d_cpu,
+    &ln_opimpl_print_cpu,
+    &ln_opimpl_rearange_cpu,
     &ln_opimpl_relu_cpu,
+    &ln_opimpl_reshape_cpu,
+    &ln_opimpl_sigmoid_cpu,
     &ln_opimpl_slice_cpu,
     &ln_opimpl_softmax_cpu,
-    &ln_opimpl_transpose_cpu,
-    &ln_opimpl_zeros_cpu,
-    &ln_opimpl_print_cpu,
-    &ln_opimpl_sigmoid_cpu,
-    &ln_opimpl_sort1d_cpu,
     &ln_opimpl_sort1d_by_key_cpu,
-    &ln_opimpl_arange_cpu,
+    &ln_opimpl_sort1d_cpu,
     &ln_opimpl_transform_bboxSQD_cpu,
-    &ln_opimpl_rearange_cpu,
-    &ln_opimpl_pick1d_cpu,
-    &ln_opimpl_fprint_cpu,
-    &ln_opimpl_lrelu_cpu,
-    &ln_opimpl_detect_yolov3_cpu,
-    &ln_opimpl_avgpool2d_cpu,
+    &ln_opimpl_transpose_cpu,
+    &ln_opimpl_upsample_cpu,
+    &ln_opimpl_zeros_cpu,
 /* end of init cpu ops */
     NULL
 };
 
-static ln_list *ep_cpu(const ln_op *op, const ln_dfg *dfg, int *match)
-{
-    ln_list *new_ops = NULL;
-    return new_ops;
-}
+extern ln_list *ln_expander_cpu_expander(const ln_op *op, const ln_dfg *dfg, int *match);
+/* end of declare cpu expanders */
 
 ln_expander_func ep_funcs_cpu[] = {
-    ep_cpu,
-};
-
-ln_combiner_func cb_funcs_cpu[] = {
+    ln_expander_cpu_expander,
+/* end of cpu expanders */
     NULL
 };
 
+/* end of declare cpu combiners */
+
+ln_combiner_func cb_funcs_cpu[] = {
+/* end of cpu combiners */
+    NULL
+};
+
+extern void ln_expander_init_cpu_expander(void **context_p);
+/* end of declare cpu init funcs */
+
+static void init_cpu(void **context_p)
+{
+    ln_expander_init_cpu_expander(context_p);
+/* end of exec cpu init funcs */
+}
+
+extern void ln_expander_cleanup_cpu_expander(void **context_p);
+/* end of declare cpu cleanup funcs */
+
+static void cleanup_cpu(void **context_p)
+{
+    ln_expander_cleanup_cpu_expander(context_p);
+/* end of exec cpu cleanup funcs */
+}
+
 ln_arch ln_archimpl_cpu = {
-    .init_func = NULL,
-    .cleanup_func = NULL,
+    .init_func = init_cpu,
+    .cleanup_func = cleanup_cpu,
     .reg_ops = ops_cpu,
     .ep_funcs = ep_funcs_cpu,
     .cb_funcs = cb_funcs_cpu,
