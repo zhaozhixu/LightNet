37f013d1d7ecb224661a6df14e6b9514fdb7bd9c
Zhao Zhixu <zhixu001@gmail.com>
2019-04-17 23:24:53 +0800

    fix typo

--- src/ln_util.h
+++ src/ln_util.h
@@ -49,7 +49,7 @@ typedef void *(*ln_copy_func)(void *dst, const void *src, size_t n);
 
 #define LN_TIMEIT_START { double _t1, _t2; _t1 = ln_clock();
 #define LN_TIMEIT_END(msg, varg...)                             \
-    _t2 = ln_clock(); ln_msg_info(msg"%.6f", ##varg, _t2 - _t1); }
+    _t2 = ln_clock(); ln_msg_info(msg"%.6fs", ##varg, _t2 - _t1); }
 
 #ifdef __cplusplus
 #define LN_CPPSTART extern "C" {
