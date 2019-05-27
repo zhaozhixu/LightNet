782c3507dff72b76331ced69bba16cf3d3f5bb73
zhixu <zzx.2013@qq.com>
2019-05-27 13:34:45 +0800

    Deployed 0dc4b0f with MkDocs version: 1.0.4

---
+++ js/baidu_analyse.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?e7e14ff1b2eabc7b939b636f5f9a9a10";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
