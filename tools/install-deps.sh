bb70ebe4f55cebc51d13887307759357b5aead15
zhixu <zhixu001@gmail.com>
2020-03-14 10:51:43 +0800

    update ci scripts

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -8,10 +8,14 @@ sudo apt-get install -y build-essential perl git pkg-config check libjpeg-dev \
 
 pip3 install -U --no-cache-dir mkdocs markdown>=3.1.1 pygments opencv-python
 
+mkdocs --version
+
 export PERL_MM_USE_DEFAULT=1
 sudo cpan install JSON Sort::strverscmp
 
+ncpu=`cat /proc/cpuinfo| grep "processor"| wc -l`
+
 wget https://github.com/zhaozhixu/TensorLight/archive/master.zip -O TensorLight.zip
 unzip TensorLight.zip
-(cd TensorLight-master && ./configure && make && make test \
+(cd TensorLight-master && ./configure && make -j$ncpu && make test -j$ncpu \
      && sudo make install && sudo ldconfig)
--- tools/run-test.sh
+++ tools/run-test.sh
@@ -3,3 +3,10 @@
 set -ev
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+
+ncpu=`cat /proc/cpuinfo| grep "processor"| wc -l`
+
+echo $ncpu
+
+./configure --with-python=yes && make -j$ncpu && make test -j$ncpu \
+    && make doc && sudo make install && sudo ldconfig
